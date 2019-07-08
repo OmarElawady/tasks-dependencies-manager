6d2fba648ec96bb9cbe1a997ba746ba3acc9c88d
Omar Elawady <omarelawady1998@gmail.com>
2019-07-08 10:23:06 +0200

    Use monkey patch to test printing functions

--- test.py
+++ test.py
@@ -1,5 +1,6 @@
 import pytest
 import make
+
 @pytest.fixture
 def task():
     return make.Task(["apt", "report", "publish"], "say hello")
@@ -38,16 +39,31 @@ def satisfies_order(constrains, execution):
     return True
 def correct_cycle(reference_cycle, out_cycle):
     return len(reference_cycle) == len(out_cycle) and set(reference_cycle) == set(out_cycle)
-def test_normal_commands(normal_task_list):
-    result = normal_task_list.print_out_to_string("a")
+
+class OutTest:
+    def __init__(self):
+        self.out = ""
+    def print(self, x):
+        self.out += x + "\n"
+    def get(self):
+        return self.out
+
+def test_normal_commands(normal_task_list, monkeypatch):
+    out = OutTest() 
+    monkeypatch.setattr("builtins.print", out.print)
+    normal_task_list.run_task("a")
+    result = out.get()
     tree = {"a": ["b", "c"],
             "b": ["d"],
             "c": [],
             "d": []}
     assert satisfies_order(tree, result.strip()), "Print commands in the correct order"
 
-def test_cyclic_commands(cyclic_task_list):
-    result = cyclic_task_list.print_out_to_string("a")
+def test_cyclic_commands(cyclic_task_list, monkeypatch):
+    out = OutTest()
+    monkeypatch.setattr("builtins.print", out.print)
+    cyclic_task_list.run_task("a")
+    result = out.get()
     cycle = ["a", "b", "d"]
     assert correct_cycle(cycle, result.strip().split('\n')[1:-1]), "Print out the task cycle"
 
